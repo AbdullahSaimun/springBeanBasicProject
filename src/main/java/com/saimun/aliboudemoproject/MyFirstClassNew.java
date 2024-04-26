1718b8f9aec1ca47ce8d2c2fe5d96a767027c341
Abdulla Al Saimun <44600426+AbdullahSaimun@users.noreply.github.com>
2024-04-26 22:49:00 +0600

    @Primary

--- src/main/java/com/saimun/aliboudemoproject/MyFirstClassNew.java
+++ src/main/java/com/saimun/aliboudemoproject/MyFirstClassNew.java
@@ -1,6 +1,11 @@
 package com.saimun.aliboudemoproject;
 
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class MyFirstClassNew {
 	public String sayHello() {
 		return "Hello from the MyFirstClass";
