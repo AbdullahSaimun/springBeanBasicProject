abaa0a0fa493306187879f9e3bf397ce03a37871
Abdulla Al Saimun <44600426+AbdullahSaimun@users.noreply.github.com>
2024-04-26 22:51:24 +0600

    bean name

--- src/main/java/com/saimun/aliboudemoproject/AliboudemoprojectApplication.java
+++ src/main/java/com/saimun/aliboudemoproject/AliboudemoprojectApplication.java
@@ -10,8 +10,8 @@ public class AliboudemoprojectApplication {
 	public static void main(String[] args) {
 		var ctx = SpringApplication.run(AliboudemoprojectApplication.class, args);
 //		MyFirstClassNew myFirstClassNew = ctx.getBean(MyFirstClassNew.class,"myFirstClassNew");
-//		MyFirstClassNew myFirstClassNew = (MyFirstClassNew) ctx.getBean("myFirstClass");
-		MyFirstClassNew myFirstClassNew = ctx.getBean(MyFirstClassNew.class);
+		MyFirstClassNew myFirstClassNew = (MyFirstClassNew) ctx.getBean("beanNameFromMyFristClass");
+//		MyFirstClassNew myFirstClassNew = ctx.getBean(MyFirstClassNew.class);
 		System.out.println(myFirstClassNew.sayHello());
 	}
 
--- src/main/java/com/saimun/aliboudemoproject/ApplicationConfig.java
+++ src/main/java/com/saimun/aliboudemoproject/ApplicationConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
-	@Bean
+	@Bean(name = "beanNameFromMyFristClass")
 	public MyFirstClassNew myFirstClass() {
 		return new MyFirstClassNew();
 	}
