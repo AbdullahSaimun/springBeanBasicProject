83103edd4ff54a88fc4c137d048d70d244fbc9b9
Abdulla Al Saimun <44600426+AbdullahSaimun@users.noreply.github.com>
2024-04-27 13:33:19 +0600

    add qualifier

--- src/main/java/com/saimun/aliboudemoproject/ApplicationConfig.java
+++ src/main/java/com/saimun/aliboudemoproject/ApplicationConfig.java
@@ -1,13 +1,22 @@
 package com.saimun.aliboudemoproject;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ApplicationConfig {
 	@Bean
+	@Qualifier("second")
 	public MyFirstClassNew myFirstClass() {
 		return new MyFirstClassNew("saimun");
+	}
+	@Bean
+//	@Primary
+	@Qualifier("first")
+	public MyFirstClassNew myFirstClassTwo() {
+		return new MyFirstClassNew("salamn");
 	}
 
 }
--- src/main/java/com/saimun/aliboudemoproject/MyFirstService.java
+++ src/main/java/com/saimun/aliboudemoproject/MyFirstService.java
@@ -1,6 +1,7 @@
 package com.saimun.aliboudemoproject;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,7 +10,7 @@ public class MyFirstService {
 	private MyFirstClassNew myFirstClass;
 
 	@Autowired
-	public MyFirstService(MyFirstClassNew myFirstClass) {
+	public MyFirstService(@Qualifier("first") MyFirstClassNew myFirstClass) {
 		this.myFirstClass = myFirstClass;
 	}
 
