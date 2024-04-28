af6a1e8d8de9fa97990184af5d2099da10785b5b
Abdulla Al Saimun <44600426+AbdullahSaimun@users.noreply.github.com>
2024-04-28 23:10:45 +0600

    set profile programatically from main method

--- src/main/java/com/saimun/aliboudemoproject/AliboudemoprojectApplication.java
+++ src/main/java/com/saimun/aliboudemoproject/AliboudemoprojectApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class AliboudemoprojectApplication {
 
 	public static void main(String[] args) {
-		var ctx = SpringApplication.run(AliboudemoprojectApplication.class, args);
+		var app = new SpringApplication(AliboudemoprojectApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active","test"));
+		var ctx = app.run( args);
 		MyFirstServiceForCustomProfiles myFirstService = ctx.getBean(MyFirstServiceForCustomProfiles.class);
 		System.out.println(myFirstService.getPropertyName());
 
