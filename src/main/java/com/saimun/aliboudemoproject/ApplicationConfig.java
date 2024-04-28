074b492e68fd64226bbde9aecbe0847089c562fb
Abdulla Al Saimun <44600426+AbdullahSaimun@users.noreply.github.com>
2024-04-28 23:16:16 +0600

    Bean only available on specific profile

--- src/main/java/com/saimun/aliboudemoproject/ApplicationConfig.java
+++ src/main/java/com/saimun/aliboudemoproject/ApplicationConfig.java
@@ -4,17 +4,20 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class ApplicationConfig {
 	@Bean
 	@Qualifier("second")
+	@Profile("dev")
 	public MyFirstClassNew myFirstClass() {
 		return new MyFirstClassNew("saimun");
 	}
 	@Bean
 //	@Primary
 	@Qualifier("first")
+	@Profile("test")
 	public MyFirstClassNew myFirstClassTwo() {
 		return new MyFirstClassNew("salamn");
 	}
